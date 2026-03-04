8344d39340f8720bdbeadb6fe2043e4186b15ad6
Nikita Jadhav <nikita.jadhav12022008@gmail.com>
2026-03-04 05:25:42 -0800

    Update and rename SWG GAME.py to snake_water_gun_game.py

--- snake_water_gun_game.py
+++ snake_water_gun_game.py
@@ -1,12 +1,14 @@
 #snake water and gun game:
+#importing random
 import random as rd
 print("enter 1 for snake/2 for water/3 for gun:")
 print("lets start the game!!!")
 print("====================")
-
+#running loop
 while True:
     a = int(input("enter your choice:"))
     b = rd.randint(1,3)
+    #conditional statements:
     if(a == b):
         print("DRAW!!")
     elif(a == 1 and b == 2):
